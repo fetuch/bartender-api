03ee0c7a37c4c2291a580dc342d956051ad552b5
Fetuch <rafal.nowak@punkgate.pl>
2023-02-03 11:02:11 +0100

    Use typeorm to create drink entity

--- src/drink.entity.ts
+++ src/drink.entity.ts
@@ -1,10 +1,25 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({ name: "drinks" })
 export class Drink {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   name: string;
+
+  @Column()
   category: string;
+
+  @Column()
   glass: string;
+
+  @Column()
   instructions: string;
-  ingredients: string[];
+
+  @Column()
   createdAt: Date;
+
+  @Column()
   updatedAt: Date;
 }
