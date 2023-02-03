3bac123c3559605dda50883bb5bf50ed611e5e3d
Fetuch <rafal.nowak@punkgate.pl>
2023-02-03 10:49:19 +0100

    Use typeorm module to connect with mysql db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { DrinksController } from './Drinks.controller';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { DrinksController } from "./Drinks.controller";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: "127.0.0.1",
+      port: 3306,
+      username: "root",
+      password: "example",
+      database: "bartender",
+    }),
+  ],
   controllers: [AppController, DrinksController],
   providers: [AppService],
 })
