3f72b3500e26b160b06af13b8d5173774cd28dec
Fetuch <rafal.nowak@punkgate.pl>
2023-02-04 18:42:41 +0100

    Move config to env files

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,20 +2,21 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { Drink } from "./drinks/drink.entity";
 import { DrinksModule } from "./drinks/drinks.module";
+import { ConfigModule } from "@nestjs/config";
+import ormConfig from "./config/orm.config";
+import ormConfigProd from "./config/orm.config.prod";
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: "mysql",
-      host: "127.0.0.1",
-      port: 3306,
-      username: "root",
-      password: "example",
-      database: "bartender",
-      entities: [Drink],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [ormConfig],
+      expandVariables: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory:
+        process.env.NODE_ENV !== "production" ? ormConfig : ormConfigProd,
     }),
     DrinksModule,
   ],
---
+++ src/config/orm.config.prod.ts
@@ -0,0 +1,17 @@
+import { registerAs } from "@nestjs/config";
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import { Drink } from "../drinks/drink.entity";
+
+export default registerAs(
+  "orm.config",
+  (): TypeOrmModuleOptions => ({
+    type: "mysql",
+    host: process.env.DB_HOST,
+    port: Number.parseInt(process.env.DB_PORT, 10),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    entities: [Drink],
+    synchronize: false,
+  })
+);
