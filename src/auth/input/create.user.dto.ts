d0aee7069d541452b3f07a4bdcabe767651366ab
Rafal Nowak <rnowak@media4u.pl>
2023-02-10 10:47:54 +0100

    Enable registration

---
+++ src/auth/input/create.user.dto.ts
@@ -0,0 +1,21 @@
+import { IsEmail, Length } from "class-validator";
+
+export class CreateUserDto {
+  @Length(3)
+  username: string;
+
+  @Length(6)
+  password: string;
+
+  @Length(6)
+  retypedPassword: string;
+
+  @Length(2)
+  firstName: string;
+
+  @Length(2)
+  lastName: string;
+
+  @IsEmail()
+  email: string;
+}
