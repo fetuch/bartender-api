69808aa2fd684bae3964119cccc908a4cbce0ebd
Rafal Nowak <rnowak@media4u.pl>
2023-02-10 10:46:20 +0100

    Compare password by bcrypt

--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -2,6 +2,7 @@ import { Injectable, Logger, UnauthorizedException } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Strategy } from "passport-local";
+import * as bcrypt from "bcrypt";
 import { Repository } from "typeorm";
 import { User } from "./user.entity";
 
@@ -26,7 +27,7 @@ export class LocalStrategy extends PassportStrategy(Strategy, "local") {
       throw new UnauthorizedException();
     }
 
-    if (password !== user.password) {
+    if (!(await bcrypt.compare(password, user.password))) {
       this.logger.debug(`Invalid credentials for user ${username}`);
       throw new UnauthorizedException();
     }
